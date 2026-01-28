314d16633b6a1e705ed3083d543636b351e581da
Khairul <khairul.web1@gmail.com>
2026-01-28 15:28:11 +0600

    user password change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
@@ -11,7 +12,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://tastTrail:qi9jbGhMz14GhBFx@cluster0.rodv5np.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,9 +27,57 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7
 
+    const usersCollection = client.db("tasteTailDB").collection("Users");
+
+    app.get("/user", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    // login system
+    app.post("/loginPoint", async (req, res) => {
+      const {email, password} = req.body;
+      const user = await usersCollection.findOne({email});
+
+      if (!user) {
+        res.send(null);
+        return
+      }
+
+      const isPassword = await bcrypt.compare(password, user?.password);
+      
+      if (isPassword) {
+        res.send(user);
+      }else{
+        res.send(null);
+      }
+      
+    })
+
+    // registration
+    app.post("/registrationRoute", async (req, res) => {
+      const data = req.body;
+
+      const existUser = await usersCollection.findOne({ email: data.email });
+      if (existUser) {
+        res.send({
+          success: false,
+          insertedId: null
+        });
+        return;
+      }
+
+      const {password, ...remain} = data;
+      const hashPassword = await bcrypt.hash(password, 10);
+      const newData = {password: hashPassword, ...remain};
+      const result = await usersCollection.insertOne(newData);
+      res.status(200).send(result);
+    })
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
   }
 }
 run().catch(console.dir);
